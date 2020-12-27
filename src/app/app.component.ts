1eb964f1c42830f0c7e49dd8e694a83831f84695
donaldteghen <donaldteghen@gmail.com>
2020-12-27 16:02:47 +0300

    Imcomplete version of the game. Game history and restart remeaining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'playApp';
+squares:string[] = Array(9).fill(null);
+gameEnd:boolean=false;
+playerwon:string;
+history = [
+ { 
+   squares : Array(9).fill(null)
+  }
+];
+current : string[]
+
+isX :boolean = true;
+nextPlayer:string = "X";
+constructor(public router:Router){}
+
+play(index:number, btn:HTMLBaseElement){
+  if(btn.getAttribute('value') != null || this.gameEnd){
+    return;
+  }
+    btn.setAttribute('value', this.isX? "X" : "O");
+    this.squares[index] = this.isX? "X" : "O";
+    this.isX =!this.isX;
+    this.nextPlayer = this.isX? "X" : "O";
+    this.winner();
+}
+
+winner(){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]) {
+      this.gameEnd = true;
+      this.playerwon = this.squares[a];
+      console.log(this.squares[a]);
+      return this.squares[a];
+    }
+  }
+  return null;
+}
+
+restart(){
+ this.gameEnd = false;
+  this.squares =Array(9).fill(null);
+  this.nextPlayer = "X";
+  this.isX = true;
+  this.playerwon = null;
+  
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, FormsModule, ReactiveFormsModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
